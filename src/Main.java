08f51e82f069d844f7b776a0392ea221d68643c3
Andrew-Bley <bleyaw@mail.uc.edu>
2022-09-20 12:49:03 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age;
+
+        System.out.println("What is your age?");
+
+        age=63;
+
+        if(age<15)
+        {
+            System.out.println("You can enter the childrens cave");
+        }
+        else
+        {
+            System.out.println("You're too old");
+        }
+
+
+    }
+}
---
+++ src/MovieLine.java
@@ -0,0 +1,14 @@
+public class MovieLine
+{
+    public static void main(String[] args)
+    {
+        int age = 67 ;
+        System.out.println("What is your age");
+
+        if(age >= 16)
+            System.out.println("You get a wristband");
+        else
+            System.out.println("You don't get a wristband");
+    }
+
+}
